85b4b66322716741b3f3dfd1814891957b6c1f40
matsushinDB11 <matsushin35@gmail.com>
2021-04-11 01:41:06 +0900

    get env

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,12 @@ import json
 
 app = Flask(__name__)
 
+# 環境変数からアクセストークンとChannel Secretをを取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 # class CoinZApi:
 #     def __init__(self):
