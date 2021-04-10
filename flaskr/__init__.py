05a083d74d04567a1a9a266c0b1289c1cbc2401a
matsushinDB11 <matsushin35@gmail.com>
2021-04-10 17:02:42 +0900

    add line-bot-sdk

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 import requests
 import json
 
 app = Flask(__name__)
+
+
+# class CoinZApi:
+#     def __init__(self):
+#         self.endpoint = 'https://api.coin.z.com/public'
+#         self.path = '/v1/ticker'
+#         self.symbol = ''
+#
+#     def set_symbol(self, symbol):
+#         self.symbol = symbol
 
 endpoint = 'https://api.coin.z.com/public'
 path = '/v1/ticker'
@@ -10,19 +24,18 @@ symbol = 'BTC'
 
 
 def make_symbol_path(symbol):
-  return path + '?symbol=' + symbol
+    return path + '?symbol=' + symbol
 
 
 @app.route('/')
 def index():
-  return 'first Page'
+    return 'second Page'
 
 
 @app.route('/btc')
 def re_json():
-  symbol_patth = make_symbol_path(symbol)
-  res = requests.get(endpoint + symbol_patth)
-  ask = json.loads(res.text)["data"][0]["ask"]
-  bid = json.loads(res.text)["data"][0]["bid"]
-  return '買値' + str(ask) + ' ' + '売値' + str(bid)
-
+    symbol_patth = make_symbol_path(symbol)
+    res = requests.get(endpoint + symbol_patth)
+    ask = json.loads(res.text)["data"][0]["ask"]
+    bid = json.loads(res.text)["data"][0]["bid"]
+    return '買値' + str(ask) + ' ' + '売値' + str(bid)
