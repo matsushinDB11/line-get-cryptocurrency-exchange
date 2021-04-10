9fd001a356b00c5de0ef2cfba4f5517114fdb79e
matsushinDB11 <matsushin35@gmail.com>
2021-04-11 02:24:49 +0900

    add callback code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,7 +26,26 @@ def make_symbol_path(symbol):
 
 @app.route('/')
 def index():
-    return 'first Page'
+    return 'line-get-cryptocurrency-exchange by Matsushin'
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
 
 
 @app.route('/btc')
